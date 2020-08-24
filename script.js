7c9d203c4165dad3791aa7acb00e19668252fe50
Daniel Khrapko <55002392+khrapkodaniel@users.noreply.github.com>
2020-08-23 22:32:53 -0400

    Updates

---
+++ script.js
@@ -0,0 +1,8 @@
+function showTab(tabName) {
+    console.log(tabName);
+
+    // for(var i = 0; i < document.getElementsByClassName("content").length; i++)
+    //     document.getElementsByClassName("content")[i].style.display = "none";
+
+    // document.getElementById(tabName).style.display = "block";
+}
