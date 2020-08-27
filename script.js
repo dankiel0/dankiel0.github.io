e58902cc74937c061631a77787d53a3da3477fbf
Daniel Khrapko <55002392+khrapkodaniel@users.noreply.github.com>
2020-08-26 21:27:23 -0400

    Updates

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function showTab(tabName) {
-    console.log(tabName);
+function onClick(tabName) {
+    for(var i = 0; i < document.getElementsByClassName("tab").length; i++)
+        document.getElementsByClassName("tab")[i].style.backgroundColor = "#FFFEF5";
+    
+    for(var i = 0; i < document.getElementsByClassName("content").length; i++)
+        document.getElementsByClassName("content")[i].style.display = "none";
+    
+    document.getElementById(tabName + "-tab").style.backgroundColor = "#FFFBD4";
+    document.getElementById(tabName + "-content").style.display = "inherit";
 
-    // for(var i = 0; i < document.getElementsByClassName("content").length; i++)
-    //     document.getElementsByClassName("content")[i].style.display = "none";
+    var url = new URL(window.location.href + "?z=" + tabName);
+    url.searchParams.set('z', tabName);
+    history.pushState({}, null, url);
+}
 
-    // document.getElementById(tabName).style.display = "block";
+function onLoad() {
+    var url = new URL(window.location.href);
+    var tabName = url.searchParams.get('z');
+    
+    if(tabName === null)
+        tabName = "about";
+    
+    document.getElementById(tabName + "-tab").style.backgroundColor = "#FFFBD4";
+    document.getElementById(tabName + "-content").style.display = "inherit";
 }
